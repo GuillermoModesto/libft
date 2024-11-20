839a5fdd9346868ce36decbdb5cbf97637c922d9
GuillermoModesto <guillermomodestoanti@gmail.com>
2024-11-20 18:07:13 +0100

    añadir atoi

---
+++ ft_atoi.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	num;
+
+	i = 0;
+	sign = 1;
+	num = 0;
+	while (nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-')
+	{
+		sign *= (-1);
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		num *= 10;
+		num += nptr[i] - 48;
+		i++;
+	}
+	num *= sign;
+	return (num);
+}
