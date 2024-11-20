b393171641349e4c40f361d03022f950feb2431e
GuillermoModesto <guillermomodestoanti@gmail.com>
2024-11-20 18:14:00 +0100

    añadir memchr

---
+++ ft_memchr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *buf, int c, int count)
+{
+	int		i;
+	char	*aux_buf;
+
+	i = 0;
+	aux_buf = (char *)buf;
+	while (i < count)
+	{
+		if (aux_buf[i] == c)
+			return (aux_buf + i);
+		i++;
+	}
+	return (NULL);
+}
