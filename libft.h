cd051bbe4d11a1dc4db3019103fb5a5810623c38
GuillermoModesto <guillermomodestoanti@gmail.com>
2024-11-20 19:53:56 +0100

    arreglar strlen y reglas de libft

--- libft.h
+++ libft.h
@@ -1,25 +1,25 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_atoi(const char *nptr);
+int		ft_atoi(const char *nptr);
 void	ft_bzero(void *s, int n);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 void	*ft_memchr(const void *buf, int c, int count);
-int	ft_memcmp(const void *buf1, const void *buf2, int count);
+int		ft_memcmp(const void *buf1, const void *buf2, int count);
 void	*ft_memcpy(void *dest, const void *src, int count);
 void	*ft_memmove(void *dest, const void *src, int count);
 void	*ft_memset(void *s, int c, int n);
 char	*ft_strchr(const char *buf, int c);
 char	*ft_strlcat(char *dst, const char *src, int size);
 char	*ft_strlcpy(char *dst, const char *src, int size);
-int	ft_strlen(const char *s);
-int	ft_strncmp(const char *str1, const char *str2, int count);
+int		ft_strlen(const char *s);
+int		ft_strncmp(const char *str1, const char *str2, int count);
 char	*ft_strrchr(const char *buf, int c);
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 
 #endif
