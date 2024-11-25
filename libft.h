04f0ffa7b96d7ca133923333bcc8a969dddadc5a
GuillermoModesto <guillermomodestoanti@gmail.com>
2024-11-25 17:45:54 +0100

    ordenar libft

--- libft.h
+++ libft.h
@@ -14,19 +14,19 @@ int		ft_strlen(const char *s);
 int		ft_strncmp(const char *str1, const char *str2, int count);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
-void	ft_bzero(void *s, int n);
-void	*ft_memchr(const void *buf, int c, int count);
-void	*ft_memcpy(void *dest, const void *src, int count);
-void	*ft_memmove(void *dest, const void *src, int count);
-void	*ft_memset(void *s, int c, int n);
 char	*ft_strchr(const char *buf, int c);
 char	*ft_strlcat(char *dst, const char *src, int size);
 char	*ft_strlcpy(char *dst, const char *src, int size);
 char	*ft_strrchr(const char *buf, int c);
 char	*ft_strnstr(const char *big, const char *little, int len);
-void	*ft_calloc(int nelem, int elsize);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_bzero(void *s, int n);
+void	*ft_memchr(const void *buf, int c, int count);
+void	*ft_memcpy(void *dest, const void *src, int count);
+void	*ft_memmove(void *dest, const void *src, int count);
+void	*ft_memset(void *s, int c, int n);
+void	*ft_calloc(int nelem, int elsize); 
 
 #endif
