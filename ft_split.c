fb03189f7872132dc85bfaca02a7df49978d8a9a
GuillermoModesto <guillermomodestoanti@gmail.com>
2024-11-28 10:35:29 +0100

    split siguiendo la norma

--- ft_split.c
+++ ft_split.c
@@ -1,47 +1,78 @@
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+int	count_words(char const *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			count++;
+		i++;
+	}
+	return (count + 1);
+}
+
+char	*create_space_word(char const *s, int start, int end)
 {
 	int		i;
-	int		j;
-	int		z;
-	int		size;
-	char	*aux;
-	char	**mt;
+	char	*word;
+
+	i = start;
+	word = (char *)malloc((end - start + 1) * sizeof(char));
+	if (word == NULL)
+		return (NULL);
+	while (i < end)
+	{
+		word[i - start] = s[i];
+		i++;
+	}
+	word[end - start] = '\0';
+	return (word);
+}
+
+void	*fill_words(char **mt, char const *s, char c)
+{
+	int	i;
+	int	j;
+	int	size;
 
 	i = 0;
 	j = 0;
 	size = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
-			size++;
-		i++;
-	}
-	size += 2;
-	mt = (char **) malloc(size * sizeof(char *));
-	i = 0;
-	size = 0;
-	while (s[i] != '\0')
-	{
-		while ((s[i] != '\0') && (s[i] != c))
+		while (s[i] == c && s[i] != '\0')
 			i++;
-		aux = (char *) malloc((i - j + 1) * sizeof(char *));
-		i++;
-		if (aux == NULL)
-			return (NULL);
-		z = 0;
-		while (j < (i - 1))
+		j = i;
+		while (s[i] != c && s[i] != '\0')
+			i++;
+		if (j < i)
 		{
-			aux[z] = s[j];
-			j++;
-			z++;
+			mt[size] = create_space_word(s, j, i);
+			if (mt[size] == NULL)
+				return (NULL);
+			size++;
 		}
-		aux[z] = '\0';
-		mt[size] = aux;
-		size++;
-		j++;
 	}
 	mt[size] = NULL;
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		size;
+	char	**mt;
+
+	if (s == NULL)
+		return (NULL);
+	size = count_words(s, c);
+	mt = (char **)malloc((size + 1) * sizeof(char *));
+	if (mt == NULL)
+		return (NULL);
+	fill_words(mt, s, c);
 	return (mt);
 }
