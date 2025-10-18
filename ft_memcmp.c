8fe80a46c1438884942fd4b18b2dd972c012a27d
Modesto <152183708+GuillermoModesto@users.noreply.github.com>
2025-10-18 16:56:38 +0200

    refactor

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -3,20 +3,17 @@
 int	ft_memcmp(const void *buf1, const void *buf2, int count)
 {
 	int		i;
-	int		n;
 	char	*aux_buf1;
 	char	*aux_buf2;
 
 	i = 0;
-	n = 0;
 	aux_buf1 = (char *)buf1;
 	aux_buf2 = (char *)buf2;
 	while (i < count)
 	{
-		n = aux_buf1[i] - aux_buf2[i];
-		if (n != 0)
-			return (n);
+		if (aux_buf1[i] != aux_buf2[i])
+			return (aux_buf1[i] - aux_buf2[i]);
 		i++;
 	}
-	return (n);
+	return (0);
 }
