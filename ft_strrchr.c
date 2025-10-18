05980a0d49e8ac7492bb9e0e2de5933692de0629
Modesto <152183708+GuillermoModesto@users.noreply.github.com>
2025-10-18 15:18:06 +0200

    refactor

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,11 +5,10 @@ char	*ft_strrchr(const char *buf, int c)
 	int	i;
 
 	i = ft_strlen((char *)buf);
-	while (i >= 0)
+	while (i--)
 	{
 		if (buf[i] == c)
 			return ((char *)(buf + i));
-		i--;
 	}
-	return (0);
+	return ((void *)0);
 }
