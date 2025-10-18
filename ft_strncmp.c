cd5283c42e52cc6840c25dff6e5dcca2a9098ad8
Modesto <152183708+GuillermoModesto@users.noreply.github.com>
2025-10-18 16:49:26 +0200

    refactor

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,15 +3,13 @@
 int	ft_strncmp(const char *str1, const char *str2, int count)
 {
 	int	i;
-	int	n;
 
 	i = 0;
 	while (i < count)
 	{
-		n = str1[i] - str2[i];
-		if (n != 0)
+		if ((str1[i] - str2[i]) != 0)
 			return (n);
 		i++;
 	}
-	return (n);
+	return (0);
 }
